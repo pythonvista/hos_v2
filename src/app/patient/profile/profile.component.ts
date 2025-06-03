5d110c0908f5664cd3fe7894db4039013a4cd7fe
pythonvista <nablintech@gmail.com>
2025-06-03 16:34:21 +0100

    x

--- src/app/patient/profile/profile.component.ts
+++ src/app/patient/profile/profile.component.ts
@@ -1,133 +1,108 @@
-import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Patient } from 'src/app/_models/patient';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ResponseMessage } from 'src/app/_models/response';
-import { environment } from 'src/environments/environment';
-import { NotifierService } from 'angular-notifier';
-import { AuthService } from 'src/app/auth.service';
-import { NotifyService } from 'src/app/shared/notify.service';
-import { PatientService } from '../patient.service';
-import { AngularEditorConfig } from '@kolkov/angular-editor';
-
+import { Component, OnInit, ViewChild, TemplateRef } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Patient } from "src/app/_models/patient";
+import { Router, ActivatedRoute } from "@angular/router";
+import { ResponseMessage } from "src/app/_models/response";
+import { environment } from "src/environments/environment";
+import { NotifierService } from "angular-notifier";
+import { AuthService } from "src/app/auth.service";
+import { NotifyService } from "src/app/shared/notify.service";
+import { PatientService } from "../patient.service";
+import { AngularEditorConfig } from "@kolkov/angular-editor";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"],
 })
 export class ProfileComponent implements OnInit {
   isLoader = false;
-  /**
- * Notifier service
- */
+  isNotSubscribed = false; // New flag for subscription status
+  errorMessage: string = ""; // To store error message
+
   private notifier: NotifierService;
 
   public showNotification(type: string, message: string): void {
     this.notifier.notify(type, message);
   }
 
-
-  @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
+  @ViewChild("modalContent", { static: true }) modalContent: TemplateRef<any>;
 
   editorConfig: AngularEditorConfig = {
-    editable: true,
-    spellcheck: true,
-    height: 'auto',
-    minHeight: '6',
-    maxHeight: 'auto',
-    width: 'auto',
-    minWidth: '0',
-    translate: 'yes',
-    enableToolbar: true,
-    showToolbar: true,
-    placeholder: 'Enter text here...',
-    defaultParagraphSeparator: '',
-    defaultFontName: '',
-    defaultFontSize: '',
-    fonts: [
-      { class: 'arial', name: 'Arial' },
-      { class: 'times-new-roman', name: 'Times New Roman' },
-      { class: 'calibri', name: 'Calibri' },
-      { class: 'comic-sans-ms', name: 'Comic Sans MS' }
-    ],
-    customClasses: [
-      {
-        name: 'quote',
-        class: 'quote',
-      },
-      {
-        name: 'redText',
-        class: 'redText'
-      },
-      {
-        name: 'titleText',
-        class: 'titleText',
-        tag: 'h1',
-      },
-    ],
-    uploadUrl: 'v1/image',
-    uploadWithCredentials: false,
-    sanitize: true,
-    toolbarPosition: 'top',
-    toolbarHiddenButtons: [
-      ['bold', 'italic'],
-      ['fontSize']
-    ]
+    /* Your existing config */
   };
 
-
-  // fixedDiseaseValue;
+  patient: Patient;
+  vitals = [];
+  verticalVitals = [];
+  horizontalVitals = [];
   problems_PastMedicalHistoryValue;
   drugHistoryValue;
   surgicalHistoryValue;
   obstetrics_GynacologyHistoryValue;
   vaccinesValue;
   allergiesValue;
-
   isButtonVisible = false;
-
   tab = 1;
-  patient: Patient;
-  vitals = [];
-  verticalVitals = [];
-  horizontalVitals = [];
-  constructor(private modal: NgbModal, private router: Router,
+
+  constructor(
+    private modal: NgbModal,
+    private router: Router,
     private activatedRoute: ActivatedRoute,
     notifier: NotifierService,
     private patientService: PatientService,
     private authService: AuthService,
     private notifyService: NotifyService
-  ) { this.notifier = notifier; }
+  ) {
+    this.notifier = notifier;
+  }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(params => {
-      console.log(params['id'])
-      this.get(params['id']);
-
+    this.activatedRoute.params.subscribe((params) => {
+      this.get(params["id"]);
     });
   }
+
   get(id) {
     this.isLoader = true;
-    this.patientService.getById(id).subscribe((res: ResponseMessage) => {
-      console.log(res.data)
-      if (res.status == '200') {
-        this.patient = res.data;
-        // this.seprateVitals(res.data.vitals);
-        this.problems_PastMedicalHistoryValue = this.patient.problems_PastMedicalHistory;
-        this.drugHistoryValue = this.patient.drugHistory;
-        this.surgicalHistoryValue = this.patient.surgicalHistory;
-        this.obstetrics_GynacologyHistoryValue = this.patient.obstetrics_GynacologyHistory;
-        this.vaccinesValue = this.patient.vaccines;
-        this.allergiesValue = this.patient.allergies;
+    this.patientService.getById(id).subscribe(
+      (res: ResponseMessage) => {
+        console.log(res);
+        if (res.status == '200') {
+          this.isNotSubscribed = false; // Reset flag
+          this.patient = res.data;
+          this.problems_PastMedicalHistoryValue =
+            this.patient.problems_PastMedicalHistory;
+          this.drugHistoryValue = this.patient.drugHistory;
+          this.surgicalHistoryValue = this.patient.surgicalHistory;
+          this.obstetrics_GynacologyHistoryValue =
+            this.patient.obstetrics_GynacologyHistory;
+          this.vaccinesValue = this.patient.vaccines;
+          this.allergiesValue = this.patient.allergies;
+          this.isLoader = false;
+        } else if (res.status == '403') {
+          // Handle not subscribed case
+          this.isNotSubscribed = true;
+          this.errorMessage = res.data.message;
+          this.isLoader = false;
+          this.showNotification("error", this.errorMessage);
+          // Optionally redirect to a subscription page
+          // this.router.navigate(['app/subscription-required']);
+        } else {
+          this.router.navigate(["app/patient"]);
+          this.isLoader = false;
+        }
+      },
+      (error) => {
         this.isLoader = false;
-      } else {
-        this.router.navigate(['app/patient']);
+        this.showNotification(
+          "error",
+          "An error occurred while fetching patient data."
+        );
+        this.router.navigate(["app/patient"]);
       }
-
-    })
+    );
   }
-
   // seprateVitals(vitals = []) {
   //   console.log('vitals ', vitals.length, vitals)
   //   this.horizontalVitals = vitals.filter(vi => {
@@ -164,13 +139,13 @@ export class ProfileComponent implements OnInit {
 
   get ImageSrc() {
     if (this.patient) {
-
       return environment.base_url + this.patient.user.image;
     }
-    return environment.base_url + 'images/user.jpg';
+    return environment.base_url + "images/user.jpg";
   }
 
-  _calculateAge(birthday) { // birthday is a date
+  _calculateAge(birthday) {
+    // birthday is a date
     if (birthday) {
       birthday = new Date(birthday);
       var ageDifMs = Date.now() - birthday.getTime();
@@ -185,64 +160,68 @@ export class ProfileComponent implements OnInit {
   }
   onClose() {
     this.modal.dismissAll();
-
   }
   onOpen() {
     this.modal.open(this.modalContent);
   }
   markAsActive() {
-
-    this.authService.toggleactive(this.patient.user._id).subscribe((res: ResponseMessage) => {
-      if (res.status == '200') {
-        this.patient.user.active = !this.patient.user.active;
-        this.notifyService.success(res.data.msg);
-      }
-    });
+    this.authService
+      .toggleactive(this.patient.user._id)
+      .subscribe((res: ResponseMessage) => {
+        if (res.status == "200") {
+          this.patient.user.active = !this.patient.user.active;
+          this.notifyService.success(res.data.msg);
+        }
+      });
   }
 
   // Updates Value
   onSaveDiseaseChange(diseaseType, value) {
     this.isButtonVisible = false;
     // console.log(diseaseType, value);
-    this.patientService.updateFixedDisease(this.patient._id, { diseaseType, value }).subscribe((res: any) => {
-      if (res.status === 200) {
-        this.showNotification('success', 'Data Updated Successfully');
-      } else {
-        this.showNotification('error', 'something went wrong');
-      }
-    });
+    this.patientService
+      .updateFixedDisease(this.patient._id, { diseaseType, value })
+      .subscribe((res: any) => {
+        if (res.status === 200) {
+          this.showNotification("success", "Data Updated Successfully");
+        } else {
+          this.showNotification("error", "something went wrong");
+        }
+      });
   }
 
   onChnageAccordian() {
-    console.log('here')
+    console.log("here");
     this.isButtonVisible = false;
   }
 
   checkTemprature(t) {
-    return (t > 37.5 || t < 36.5) ? true : false;
+    return t > 37.5 || t < 36.5 ? true : false;
   }
 
   checkPulseRate(p) {
-    return (p > 100 || p < 60) ? true : false;
+    return p > 100 || p < 60 ? true : false;
   }
 
   checkCyclesPerMin(c) {
-    return (c > 18 || c < 12) ? true : false;
+    return c > 18 || c < 12 ? true : false;
   }
 
   checkSystolic(s) {
-    return (s > 139 || s < 90) ? true : false;
+    return s > 139 || s < 90 ? true : false;
   }
 
   checkDiastolic(d) {
-    return (d > 89 || d < 60) ? true : false;
+    return d > 89 || d < 60 ? true : false;
   }
 
   checkOxygen(o) {
-    return (0 > 100 || o < 92) ? true : false;
+    return 0 > 100 || o < 92 ? true : false;
   }
 
   goToEdit() {
-    this.router.navigate(['/app/patient/edit/', this.patient._id], { queryParams: { returnUrl: this.router.url } });
+    this.router.navigate(["/app/patient/edit/", this.patient._id], {
+      queryParams: { returnUrl: this.router.url },
+    });
   }
 }
